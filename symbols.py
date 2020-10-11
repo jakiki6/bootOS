ebbfa598f9e131525151abea58e2b9fee38d8944
Jakob Kirsch <jakob.kirsch@web.de>
2020-10-11 17:52:09 +0200

    ah fixed symbols gen

--- symbols.py
+++ symbols.py
@@ -16,12 +16,12 @@ with open(sys.argv[1], "r") as file:
             line = line.split(":")[0]
             line = line.strip().replace(" ", "").replace("\t", "")
             if not "equ" in ln.split(";")[0]:
-                content += f"%assign __{line} ($ - $$)\n%warning {line} at __{line}\n"
+                content += f"%assign __{line} ($ - $$)\n%warning {line} at __{line} n\n"
             else:
                 l = ln.split(";")[0].strip()
                 l = l.split(":")[1]
                 l = l.replace("equ", "").strip()
-                content += f"%assign __{line} {l}\n%warning {line} at __{line}\n"
+                content += f"%assign __{line} {l}\n%warning {line} at __{line} e\n"
         elif "org" in line:
             line = line.replace("org", "").strip()
             org = eval(line)
@@ -45,12 +45,15 @@ with open("tmp.txt", "r") as file:
             continue
         line = line.split(":")[3].strip()
         line = line.replace("[-w+user]", "").strip()
+        l = line.split(" ")
+        line = " ".join(l[:-1])
         couple = line.split(" at ")
         try:
             couple[1] = int(couple[1])
         except:
             continue
-        couple[1] += org
+        if l[-1] == "n":
+            couple[1] += org
         if couple[0].startswith("."):
             couple[0] = last + couple[0]
         else:
